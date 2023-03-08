0807f9d1149886ce62c5958c8bf8497ac8a746ed
Federico Cendali <federicocendali@gmail.com>
2023-03-08 01:18:45 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Profesor Conrado Lanusse (the best)
         </p>
+        <h2>Coderhouse inc.</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React!!!
         </a>
       </header>
     </div>
